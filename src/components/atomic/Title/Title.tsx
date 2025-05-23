a3bea82e32ad52064e2e8c714c27300776d6c93c
Daviny Letícia Vidal <daviny.vidal@outlook.com>
2025-05-22 23:35:03 -0300

    fix (Title) ajust font size

--- src/components/atomic/Title/Title.tsx
+++ src/components/atomic/Title/Title.tsx
@@ -10,7 +10,7 @@ type TitleProps = {
 export default function Title ({ text  }: TitleProps) {
   return (
   <div className={styles.container}>
-    <h1 className={styles.mainTitle}>{text.mainTitle}</h1>
+    <span className={styles.mainTitle}>{text.mainTitle}</span>
     <p className={styles.subTitle}>{text.subTitle}</p>
   </div>)
 };
